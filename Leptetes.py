37a459c3c280c06b5515376c7166e17118eb23f2
Sancy21 <90603335+Sancy1021@users.noreply.github.com>
2021-10-19 16:03:53 +0200

    Add files via upload

---
+++ Leptetes.py
@@ -0,0 +1,19 @@
+for i in range(0, 801):
+    print (i)
+
+for a in range(100, 801):
+    print (a)
+
+for b in range (0, 11):
+    print (b)
+
+for c in range(0,801,2):
+    print (c)
+
+for d in range(0,801,3):
+    print (d)
+
+    a=0
+for i in range(1,6):
+    a=a+c
+    print (a)
