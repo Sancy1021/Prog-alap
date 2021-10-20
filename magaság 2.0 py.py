eeccb87b727279f5491315849c634134cfba19de
Sancy21 <90603335+Sancy1021@users.noreply.github.com>
2021-10-20 16:38:22 +0200

    Add files via upload

---
+++ magaság 2.0 py.py
@@ -0,0 +1,8 @@
+cm = int(input('Magaság: '))
+
+if (cm < 170):
+    print('170 alatt te egy törpe vagy.')
+elif(cm == 170,180 ):
+    print('170-180 körül átlagos vagy.')
+else:
+    print('magaság 180 akkor te egy zsiráf vagy.')
