95140e88bf25311522080d4faccfbd066c2c388f
Sancy21 <90603335+Sancy1021@users.noreply.github.com>
2021-10-20 14:26:30 +0200

    Add files via upload

---
+++ Doga.py
@@ -0,0 +1,9 @@
+a = int(input(' Adj meg egy számot '))
+b = int(input(' Adj meg egy még egy számot '))
+
+print(' A két szám összege' ,a + b)
+print(' A két szám szorzata' ,a * b)
+print(' A két szám külömbsége' ,a / b)
+print(' A két szám hányadosa' ,a - b)
+
+
