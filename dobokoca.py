61118961aa0d44dac7c584d2b9bca0a4f8a5ea5d
Sancy21 <90603335+Sancy1021@users.noreply.github.com>
2021-11-03 14:45:10 +0100

    Add files via upload

---
+++ dobokoca.py
@@ -0,0 +1,36 @@
+import random
+
+a1 = random.randrange(1,7)
+print(a1)
+if a1 == 1:
+    print(a1)
+    print('...')
+    print('.*.')
+    print('...')
+    print(a1)
+elif a1== 2:
+    print(a1)
+    print('...')
+    print('*.*')
+    print('...')
+elif a1== 3:
+    print(a1)
+    print('..*')
+    print('.*.')
+    print('*..')
+elif a1== 4:
+    print(a1)
+    print('*.*')
+    print('...')
+    print('*.*')
+elif a1==5:
+    print(a1)
+    print('*.*')
+    print('.*.')
+    print('*.*')
+else: a1==6
+print(a1)
+print('*.*')
+print('*.*')
+print('*.*')
+
