10825f4e39ec12eb0a7ecc3a5f0a5645912ba8a2
Sancy21 <90603335+Sancy1021@users.noreply.github.com>
2022-03-23 08:37:13 +0100

    Add files via upload

---
+++ Honapok.py
@@ -0,0 +1,14 @@
+from ntpath import join
+
+
+Honapok = ['jan','feb','marc','april']
+print(Honapok)
+
+join
+print(','.join(Honapok))
+print(len(Honapok))
+print(Honapok[0])
+print(Honapok[3])
+print(Honapok[1:3])
+print(Honapok[:3])
+print(Honapok[2:])
